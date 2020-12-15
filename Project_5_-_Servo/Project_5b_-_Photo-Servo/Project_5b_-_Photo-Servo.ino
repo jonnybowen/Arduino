15c5cc7ea9e476c2660c794cd75f6c91b6685bfc
jonnybowen <60743016+jonnybowen@users.noreply.github.com>
2020-12-15 16:07:01 +0000

    Project 5b - Photo-Servo
    Variation of basic servo project.
    Servo now operates based on values received from a phototransistor instead of a potentiometer.
    
    Included circuit diagram

---
+++ Project_5_-_Servo/Project_5b_-_Photo-Servo/Project_5b_-_Photo-Servo.ino
@@ -0,0 +1,43 @@
+//Learning how to control a servo with a photo-transistor.
+
+#include <Servo.h> // Servo Library
+
+Servo myServo; //instantiate servo object
+
+//Pins
+int const photoPin = A0;   // The photo-transistor pin
+
+//vars
+//int photoRaw; // the raw input value from the photo-transistor
+int photoVal; // the processed input value from the photo-transisotr
+int angle;  // the angle to set the servo
+
+
+void setup() {
+  
+  myServo.attach(9); // the pin to which the servo is attached
+  Serial.begin(9600); // open a serial stream @ 9600 b/s
+
+}
+
+void loop() {
+
+  photoVal = analogRead(photoPin);
+  delay(5);
+
+  Serial.print("Photo-Val: "); //Print Serial Monitor Values
+  Serial.print(photoVal);
+
+  angle = map(photoVal, 0, 1023, 0, 179); //map photo-transistor values to 0-180 degrees
+
+
+  Serial.print(", angle: "); //Print Serial Monitor Values
+  Serial.println(angle);
+
+  myServo.write(angle); // 
+  delay(15); // 15ms delay until next reading
+
+
+  
+
+}
